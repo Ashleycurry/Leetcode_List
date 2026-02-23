6c9195a1817ce5a12f6ab07d4c3f3069a6602fe5
Xiaogao <aashleycurry@icloud.com>
2026-02-23 17:48:03 +0800

    26/02/23

---
+++ src/数组/121.买卖股票的最佳时机.c
@@ -0,0 +1,11 @@
+int maxProfit(int* prices, int pricesSize) {
+    int max = 0,min = prices[0];
+    for(int i = 1; i < pricesSize; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        } else if(prices[i] - min > max){
+            max = prices[i] - min;
+        }
+    }
+    return max;
+}
