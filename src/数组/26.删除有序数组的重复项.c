4780836a07dba80ccfebfac3b315d2bfc276b933
Xiaogao <aashleycurry@icloud.com>
2026-02-23 15:08:30 +0800

    26/02/23

---
+++ src/数组/26.删除有序数组的重复项.c
@@ -0,0 +1,10 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int slow = 0;
+    for(int fast = 1; fast < numsSize; fast++){
+        if(nums[slow] != nums[fast]){
+            slow++;
+            nums[slow] = nums[fast];
+        }
+    }
+    return slow + 1;
+}
