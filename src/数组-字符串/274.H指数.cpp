7a829a9d2debda45e4ae6f4771906c82328fabb2
Xiaogao <aashleycurry@icloud.com>
2026-03-02 22:53:51 +0800

    26/03/02

---
+++ src/数组-字符串/274.H指数.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& cs) {
+        //降序排序
+        sort(cs.begin(), cs.end(), [](const int &a, const int &b){ return a > b; });
+        int h = 0;
+        for(int i = 0; i < cs.size(); i++){
+            if(cs[i] >= i + 1){
+                h = i + 1;
+            } else {
+                break;
+            }
+        }
+        return h;
+    }
+};
